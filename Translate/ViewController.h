f8d8cad8caa8ba58bb2178f0732b3780859b57d2
忠恕 <hemiao.hm@alibaba-inc.com>
2015-05-12 00:33:26 +0800

    Initial Commit

---
+++ Translate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Translate
+//
+//  Created by zhongshu on 15-5-12.
+//  Copyright (c) 2015年 NightComing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
